4b9a0ca26a182b2a82b490d6fcb4fa3d558d8def
adesharakeval <adesharakeval@gmail.com>
2024-04-21 15:33:42 +0530

    code commit

---
+++ Locker_Thread_5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Locker_Thread_5
+{
+    class ThreadSafe
+    {
+        static bool done;
+        static object locker = new object();
+        static void Main(string[] args)
+        {
+            new Thread(Go).Start();
+            Go();
+            Console.ReadLine();
+        }
+        static void Go()
+        {
+            lock (locker)
+            {
+                if (!done)
+                {
+                   // done = true;
+                    Console.WriteLine("Done");
+                    done = true;
+                }
+            }
+        }
+    }
+}
